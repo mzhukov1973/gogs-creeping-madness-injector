333b90164bbc06ca1f28cb7936b515a11da9cac9
Gogs <gogs@fake.local>
2018-05-11 22:53:39 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# gogs-creeping-madness-injector
+# gogs-creeping-madness-injector <small><small></small>*Brain-teasing, Sanity-tilting, Subtle Repository Augmentation*</small></small>
+
 A script, to be injected into GOGS installation ([Gogs](https://github.com/gogits/gogs) is a painless self-hosted Git service.), that after a little while begins to screw with your mind. Especially effective during long hours spent in front of the computer: all-night coding sessions, work / academic emergencies, etc.. Not for the fainthearted.
+
+This is a 100% harmless script, (as opposed to a hack) written entirely according to relevant GOGS documentation, namely the [Custom Template](https://gogs.io/docs/features/custome_template) section, where the proper procedure to inject your own custom header and footer into a working Gogs installation is described.
+
+It doesn't contaminate namespace (apart from a very few globals), doesn't slow down normal work process (it kicks in after a set period of inactivity, by default - after 40 seconds), installation is trivial (you just copy one file and create or modify another), all resulting changes are non-breaking and so far no adverse effects on users work have been detected.
+
+You can fine-tune it to your tastes (parameters and a short description can be found at the top of long_hours.js file), though I sincerely recommend that you at least try it out for a short while with original settings.. 
+
+Of course everyone is different, but I think I've managed to nail the parameters required for the most pyschedelic effect.. ))))
+
+Check out the working [demo](https://testbed2.clous.tilaa.com:7443). 
+
+Effect can be observed anywhere in the repository, regardless of wether you are logged in or not.
+
+Just sit quietly for 40 seconds. )
+
+So, the gist:
+
+Once injected, script scans the current GOGS page (every page, wherever you go inside Gogs, since it sits in its footer) for elements, suitable to its task, sets up a (40 second) timer and waits.
+
+Any UI activity during that period (for now it's only mousemove, but I'll add other events soon enough) resets the timer, so it times out only after 40 solid seconds of inactivity (hopefully at the point where a weary and porobably somewhat sleepy user stops or slows down working for a few moments to stretch/rub tired eyes/spend a few minutes staring dumbly at the monitor with unseeing eyes (truth be told, this last one is my personal favourite )) )).
+
+Once its timer timed out, it sets up another listener to capture ui events (again, so far it's only mousemove) so that it can instantly react to any user activity by bring UI back to its current condition, and starts randomly and veeery slowly rotating (in 3D) and translating (also in 3D) various page elements.
+
+Changes are miniscule (rotation is limited to &#x00B1;1 degrees around each axis and translation - to &#x00B1;0.3em in each of the three directions) and slow (transitions fire randomly for each element every 4 to 8 seconds and take 4 to 8 seconds to play out), so hopefully user would mistake the suddenly slightly alive UI for his/her failing eyes or assume that they just fancy it all...) 
+
+If/when any UI action is attempted (i.e. a mouse moves inside Gogs window), script instantly ceases operation, returns page to the state it has found it initially and starts a new wait for the next 40 seconds of inactivity.
+
+Hopefully, all this produces a few more smiles for this dreary old world.... )
+
+Installation:
+
+0. Pre-requisites:
+ - A working Gogs installation. (If you don't have one - try it out! No, really - it's light, feature rich and a pleasure to work with. It's like your own personal private GitHub, only free.)
+
+1. Installation itself:
+ - Copy ```long_hours.js``` to your Gogs installation directory tree, namely to public/js subdirectory in Gogs installations' root. (By default, if it was installed according to [these](https://www.howtoforge.com/tutorial/how-to-install-gogs-go-git-service-on-ubuntu-1604) convenient instructions, it should reside in your home directory, like this: ```/home/<username>/go/src/github.com/gogits/gogs/public/js```.
+ - Similarly, copy ```long_hours.css``` to ```<gogs_home_dir>/public/css```.
+ - If you've already done some customisations to your Gogs installation (i.e. if the files ```<gogs_home_dir>/custom/templates/inject/footer.tmpl``` and ```<gogs_home_dir>/custom/templates/inject/head.tmpl``` already exist), then add these lines to each opf them:
+   - to ```head.tmpl```:
+
+```html
+        <link rel='stylesheet' href='/css/long_hours.css'>
+```
+
+   - and to ```footer.tmpl```:
+
+```html
+        <script src='/js/long_hours.js'></script>
+```
+ - And if you haven't and these files do not yet exist, than create and fill them like this:
+
+```console
+        cd <gogs_home_dir>
+        mkdir -p ./custom/templates/inject
+        echo "<script src='/js/long_hours.js'></script>" > ./custom/templates/inject/footer.tmpl
+        echo "<link rel='stylesheet' href='/css/long_hours.css'>" > ./custom/templates/inject/head.tmpl
+```
+
+ - Restart Gogs:
+
+```console
+        sudo service gogs restart
+```
+And you are all set! ) 
+
+Now if you or any other user of your Gogs installation idle in front of it for longer than 40 seconds in a row, you may (hopefully) spent some quality time, questioning your eyesight and/or sanity... )
+
+Enjoy! ))
---
+++ gogs_home_dir/public/js/long_hours.js
@@ -0,0 +1,176 @@
+/*    ╔════════════════════════════════════════════════════════════════════════════════════════╗    */
+/*    ║      "Long Hours" ─ Brain-teasing, Sanity-tilting, Subtle Repository Augmentation      ║    */
+/*    ║    ────────────────────────────────────────────────────────────────────────────────    ║    */
+/*    ║                                                                                        ║    */
+/*    ╠════════════════════════════════════════════════════════════════════════════════════════╣    */
+/*    ║   Author:  Maxim Zhukov <mzhukov31415dev@gmail.com>                                    ║    */
+/*    ║   GitHub:  https://github.com/mzhukov1973/gogs-creeping-madness-injector               ║    */
+/*    ║   Demo:    https://testbed2.cloud.tilaa.com:7443                                       ║    */
+/*    ║   License: MIT                                                                         ║    */
+/*    ╟────────────────────────────────────────────────────────────────────────────────────────╢    */
+/*    ║     MIT License                                                                        ║    */
+/*    ║                                                                                        ║    */
+/*    ║     Copyright (c) 2018 Maxim Zhukov                                                    ║    */
+/*    ║                                                                                        ║    */
+/*    ║     Permission is hereby granted, free of charge, to any person obtaining a copy       ║    */
+/*    ║     of this software and associated documentation files (the "Software"), to deal      ║    */
+/*    ║     in the Software without restriction, including without limitation the rights       ║    */
+/*    ║     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell          ║    */
+/*    ║     copies of the Software, and to permit persons to whom the Software is              ║    */
+/*    ║     furnished to do so, subject to the following conditions:                           ║    */
+/*    ║                                                                                        ║    */
+/*    ║     The above copyright notice and this permission notice shall be included in all     ║    */
+/*    ║     copies or substantial portions of the Software.                                    ║    */
+/*    ║                                                                                        ║    */
+/*    ║     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR         ║    */
+/*    ║     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,           ║    */
+/*    ║     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE        ║    */
+/*    ║     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER             ║    */
+/*    ║     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,      ║    */
+/*    ║     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE      ║    */
+/*    ║     SOFTWARE.                                                                          ║    */
+/*    ╠════════════════════════════════════════════════════════════════════════════════════════╣    */
+/*    ║   Parameters:                                                                          ║    */
+/*    ║    ────────────────────────────────────────────────────────────────────────────────    ║    */
+/*    ║       The maximum amount (in degrees) any affected element shall be rotated            ║    */
+/*    ║     relative to each of the three axes, each time its' timer fires.                    ║    */
+/*    ║       I.e. the element would: "transform:rotateX(±1°) rotateY(±1°) rotateZ(±1°)"       ║    */
+/*    ║     using 3 random numbers [-1°; +1°] as the amount to rotate around each of           ║    */
+/*    ║     the 3 axes.                                                                        ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const rotAmpl = 1;               /*│  Default: 1  (±1°)                        ║    */
+/*    ║     ╰──────────────────────────────────────╯                                           ║    */
+/*    ║                                                                                        ║    */
+/*    ║       The maximum amount (in em) any affected element would be translated along each   ║    */
+/*    ║     of the three axes, each time its attached timer fires.                             ║    */
+/*    ║       I.e. the element would "transform:translate3D(±0.3ᴱᴹ, ±0.3ᴱᴹ, ±0.3ᴱᴹ)"           ║    */
+/*    ║     using 3 random numbers [-0.3ᴱᴹ; +0.3ᴱᴹ] as the measure of how far to move along    ║    */
+/*    ║     each of the axes.                                                                  ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const transAmpl = 0.3;           /*│  Default: 0.3  (±0.3ᴱᴹ)ₑₘᵉᵐͤͫʰ🄴 🄼 🅔 🅜            ║    */
+/*    ║     ╰──────────────────────────────────────╯                                     🅴 🅼 🄔 🄜   ║    */
+/*    ║                                                                                        ║    */
+/*    ║       The minimum and maximum amount of time (in seconds) each new transition is       ║    */
+/*    ║     to take.                                                                           ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const transitMin = 4;            /*│  Default: 4  (⩾4sec)                      ║    */
+/*    ║     │*/ const transitMax = 8;            /*│  Default: 8  (⩽8sec)                      ║    */
+/*    ║     ╰──────────────────────────────────────╯                                           ║    */
+/*    ║                                                                                        ║    */
+/*    ║       The minimum and maximum amount of time (in seconds) that would elapse since      ║    */
+/*    ║     UI animation has commenced until each affected element would start its first       ║    */
+/*    ║     transformation.                                                                    ║    */
+/*    ║       Random part is set much larger for the wait time until the first                 ║    */
+/*    ║     transformation, because we want to attract as little attention as possible         ║    */
+/*    ║     and spread the mass beginning of motion across relatively large period.            ║    */
+/*    ║       This affects only the first transformation.                                      ║    */
+/*    ║       Once it is complete, every next one happens much faster, since there's no        ║    */
+/*    ║     'stillness'/'motion' visual threshold to attract users' attention.                 ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const firstTransfWaitMin = 3;    /*│  Default: 3   (⩾3sec)                     ║    */
+/*    ║     │*/ const firstTransfWaitMax = 20;   /*│  Default: 20  (⩽20sec)                    ║    */
+/*    ║     ╰──────────────────────────────────────╯                                           ║    */
+/*    ║                                                                                        ║    */
+/*    ║       The minimum and maximum amount of time (in seconds) that would elapse between    ║    */
+/*    ║     two consequitive transformations of each affected element for all transformations  ║    */
+/*    ║     after the first one.                                                               ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const transfWaitMin = 4;         /*│  Default: 4  (⩾4sec)                      ║    */
+/*    ║     │*/ const transfWaitMax = 8;         /*│  Default: 8  (⩽8sec)                      ║    */
+/*    ║     ╰──────────────────────────────────────╯                                           ║    */
+/*    ║                                                                                        ║    */
+/*    ║       The amount of time there should be no Gogs-related UI activity for animation     ║    */
+/*    ║     to launch.                                                                         ║    */
+/*    ║     ╭──────────────────────────────────────╮                                           ║    */
+/*    ║     │*/ const preStartDelay = 40;        /*│  Default: 40  (40sec)                     ║    */
+/*    ║     ╰──────────────────────────────────────╯                                           ║    */
+/*    ║                                                                                        ║    */
+/*    ╚════════════════════════════════════════════════════════════════════════════════════════╝    */
+/*   ╤
+/* ╟─┼─╢
+/*   │
+
+
+/*==============================================================================================================================================================================*/
+
+
+//  ⟪⟪⟪⟪⟪⟪⟪⟪  ⟫⟫⟫⟫⟫⟫⟫⟫
+//  ««««««««  »»»»»»»»
+//   left-top-right:     ╔═╤═╗
+// left-middle-right:    ╟─┼─╢
+// left-middle-right:    ║ │ ║
+// left-middle-right:    ╟─┼─╢
+// left-bottom-right:    ╚═╧═╝
+
+
+
+const transitRnd         = transitMax         - transitMin;
+const firstTransfWaitRnd = firstTransfWaitMax - firstTransfWaitMin;
+const transfWaitRnd      = transfWaitMax      - transfWaitMin;
+
+const tRandom = () => (window.crypto.getRandomValues(new Uint16Array(1))[0]/65536).toFixed(6);
+
+const recurToutPt = function(el) {
+  el.style.transition = 'all '+(transitMin+tRandom()*transitRnd).toFixed(6)+'s ease';
+  el.style.transform = 'rotateX('+parseInt(tRandom()*rotAmpl*2-rotAmpl,10)+'deg)    rotateY('+parseInt(tRandom()*rotAmpl*2-rotAmpl,10)+'deg) rotateZ('+parseInt(tRandom()*rotAmpl*2-rotAmpl,10)+'deg) translate3d('+(tRandom()*transAmpl*2-transAmpl).toFixed(6)+'em,'+(tRandom()*transAmpl*2-transAmpl).toFixed(6)+'em,'+(tRandom()*transAmpl*2-transAmpl).toFixed(6)+'em)';
+  if (!el.hasOwnProperty('lab3dTim')) {el.lab3dTim = setTimeout(recurToutPt,parseInt(1000*firstTransfWaitMin+tRandom()*1000*firstTransfWaitRnd,10),el);}
+  else {clearTimeout(el.lab3dTim); el.lab3dTim = setTimeout(recurToutPt,parseInt(1000*transfWaitMin+tRandom()*1000*transfWaitRnd,10),el);}
+}
+
+const my3DStart = () => {
+  Object.values(document.getElementsByClassName('ui label')).map(
+    el => {
+      el.parentNode.classList.add('labelContPersp');
+      el.classList.add('label3D');
+      recurToutPt(el);
+    }
+  );
+  Object.values(document.getElementsByClassName('ui button')).map(
+    el => {
+      el.parentNode.classList.add('labelContPersp');
+      el.classList.add('label3D');
+      recurToutPt(el);
+    }
+  );
+  Object.values(document.getElementsByClassName('item')).map(
+    el => {
+      if (el.tagName!=='LI') {
+        el.parentNode.classList.add('labelContPersp');
+        el.classList.add('label3D');
+        recurToutPt(el);
+      }
+    }
+  );
+}
+
+const mousemoveHandler = (ev) => {
+  Object.values(document.querySelectorAll('.labelContPersp')).map(el=>el.classList.remove('labelContPersp'));
+  let iLst = Object.values(document.querySelectorAll('.label3D')).map(el=>{
+    el.style.transition = 'all 0s ease 0s'; 
+    el.style.transform  = 'none';
+    el.classList.remove('label3D');
+    if (el.hasOwnProperty('lab3dTim')) {
+      clearTimeout(el.lab3dTim);
+//      delete el.lab3dTim;
+    }
+  });
+
+  if ((typeof window.myMouseTim)==='undefined') {
+    window.myMouseTim = setTimeout(my3DStart,1000*preStartDelay);
+  } else {
+    clearTimeout(window.myMouseTim);
+    window.myMouseTim = setTimeout(my3DStart,1000*preStartDelay);
+  }
+}
+
+const addMyEvList = () => {
+  window.document.addEventListener('mousemove',mousemoveHandler,{'capture':true,'once':false,'passive':true});
+  window.myEvList = true;
+}
+/*================================================================================================*/
+//Init and run the injected code:
+if (!window.myEvList) {
+  addMyEvList();
+}
+
+window.myMouseTim = setTimeout(my3DStart,1000*preStartDelay);
